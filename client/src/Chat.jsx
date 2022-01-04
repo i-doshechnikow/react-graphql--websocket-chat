9fb7734cc27ee85aed5b6f1fdd79a1b1dbd0af4b
i-doshechnikowa <iliya.yurievich@yandex.ru>
2022-01-04 21:23:57 +0300

    create request with useQuery, add easy style for dialog

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -1,14 +1,64 @@
 import React from "react";
 
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { useQuery, gql } from "@apollo/client";
+
+import { Container } from "shards-react";
 
 const client = new ApolloClient({
   uri: "http://localhost:4000/",
   cache: new InMemoryCache(),
 });
 
+const GET_MESSAGES = gql`
+  query {
+    messages {
+      id
+      user
+      content
+    }
+  }
+`;
+
+const Messages = (props) => {
+  const { user } = props;
+  const { loading, error, data } = useQuery(GET_MESSAGES);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  return (
+    <>
+      {data.messages.map(({ id, user: messageUser, content }) => (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: user === messageUser ? "flex-end" : "flex-start",
+            paddingBottom: "1em",
+          }}
+        >
+          <div
+            style={{
+              background: user === messageUser ? "#58bf56" : "#e5e6ea",
+              color: user === messageUser ? "white" : "black",
+              padding: "1em",
+              borderRadius: "1em",
+              maxWidth: "60%",
+            }}
+          >
+            {content}
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};
+
 const Chat = () => {
-  return <div>its chat</div>;
+  return (
+    <Container>
+      <Messages user={"iliya"} />
+    </Container>
+  );
 };
 
 export default () => (
