a62e14e6c9bf0c27c93cf2f123cc98e1b6d483fe
i-doshecnikow <iliya.yurievich@yandex.ru>
2022-01-05 15:46:06 +0300

    add mutation and polling 500ms as temporary solution

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
-import { useQuery, gql } from "@apollo/client";
+import { useQuery, gql, useMutation } from "@apollo/client";
 
-import { Container } from "shards-react";
+import { Container, Row, Col, FormInput, Button } from "shards-react";
 
 const client = new ApolloClient({
   uri: "http://localhost:4000/",
@@ -20,9 +20,17 @@ const GET_MESSAGES = gql`
   }
 `;
 
+const POST_MESSAGE = gql`
+  mutation ($user: String!, $content: String!) {
+    postMessage(user: $user, content: $content)
+  }
+`;
+
 const Messages = (props) => {
   const { user } = props;
-  const { loading, error, data } = useQuery(GET_MESSAGES);
+  const { loading, error, data } = useQuery(GET_MESSAGES, {
+    pollInterval: 500,
+  });
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error :(</p>;
 
@@ -36,6 +44,22 @@ const Messages = (props) => {
             paddingBottom: "1em",
           }}
         >
+          {user !== messageUser && (
+            <div
+              style={{
+                height: 50,
+                width: 50,
+                marginRight: "0.5em",
+                border: "2px solid #e5e6ae",
+                borderRadius: 25,
+                textAlign: "center",
+                fontSize: "18pt",
+                paddingTop: 5,
+              }}
+            >
+              {messageUser.slice(0, 2).toUpperCase()}
+            </div>
+          )}
           <div
             style={{
               background: user === messageUser ? "#58bf56" : "#e5e6ea",
@@ -54,9 +78,59 @@ const Messages = (props) => {
 };
 
 const Chat = () => {
+  const [state, setState] = useState({
+    user: "iliya",
+    content: "",
+  });
+
+  const [postMessage] = useMutation(POST_MESSAGE);
+
+  const onSend = () => {
+    if (state.content.length > 0) {
+      postMessage({
+        variables: state,
+      });
+    }
+    setState({ ...state, content: "" });
+  };
+
   return (
     <Container>
-      <Messages user={"iliya"} />
+      <Messages user={state.user} />
+      <Row>
+        <Col xs={2} style={{ padding: 0 }}>
+          <FormInput
+            label="user"
+            value={state.user}
+            onChange={(event) =>
+              setState({
+                ...state,
+                user: event.target.value,
+              })
+            }
+          />
+        </Col>
+        <Col xs={8}>
+          <FormInput
+            label="message"
+            value={state.content}
+            onChange={(event) =>
+              setState({
+                ...state,
+                content: event.target.value,
+              })
+            }
+            onKeyUp={(event) => {
+              if (event.keyCode === 13) {
+                onSend();
+              }
+            }}
+          />
+        </Col>
+        <Col xs={2} style={{ padding: 0 }}>
+          <Button onClick={() => onSend()}>Send</Button>
+        </Col>
+      </Row>
     </Container>
   );
 };
